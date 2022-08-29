0c3b09d7e6006e7fd87da42e328230288ec0cb2a
chambritas <salavdorjurado49@hotmail.com>
2022-08-29 10:14:51 -0600

    mensaje de error al firmar declaracion

--- src/app/presentar-declaracion/shared-presentar-declaracion/section-footer/section-footer.component.ts
+++ src/app/presentar-declaracion/shared-presentar-declaracion/section-footer/section-footer.component.ts
@@ -100,7 +100,7 @@ export class SectionFooterComponent implements OnInit {
           });
         } catch (error) {
           console.log(error);
-          this.openSnackBar('ERROR: No pudo firmar la declaración', 'Aceptar');
+          this.openSnackBar('ERROR: No pudo firmar la declaración Favor de verificar que todos los campos estén capturados', 'Aceptar');
         }
       }
     });
