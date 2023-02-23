bf10a2cea6ac284522decd7c621d8b2bb7ac0633
= <=>
2023-02-23 15:48:00 -0600

    probando modificar objetos en bienesInmuebles

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -429,9 +429,9 @@ export class BienesInmueblesComponent implements OnInit {
     let valores: any = [];
     let valor = {};
     for (let i = 0; i < bienInmueble.length; i++) {
-      console.log("entra for")
+      console.log("entra for " + bienInmueble.titular[0].clave);
       if (bienInmueble[i].titular[0].clave === "DEC") {
-        console.log("entra if")
+        console.log("entra if "+ i);
         valor = {
           "indice": i,
           "superficieConstruccion": bienInmueble[i].superficieConstruccion.valor,
