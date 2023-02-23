8025df0296ac89121985edaa9009b8a89146971f
= <=>
2023-02-23 09:57:55 -0600

    probando eliminar objetos en bienesInmuebles

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -426,8 +426,11 @@ export class BienesInmueblesComponent implements OnInit {
     let superficieConstruccion: any = {};
     let superficieTerreno: any = {};
     let valorAdquisicion: any = {};
+    console.log("llega")
     for (let i = 0; i <= this.bienInmueble.length; i++) {
+      console.log("llega a FOR")
       if (this.bienInmueble[i].titular[0].clave === "DEC") {
+        console.log("llega a IF")
         superficieConstruccion = {
           "indice": i,
           "valor": this.bienInmueble[i].superficieConstruccion.valor
