bea642403e213d64327819ce35e643867474530b
= <=>
2023-06-29 13:08:27 -0600

    modificando el signunp

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -58,8 +58,6 @@ export class SignupComponent implements OnInit, OnDestroy {
       };
     }
 
-    console.log(this.signupForm.curp);
-
     const signup$ = this.authenticationService.signup(signupForm);
     signup$
       .pipe(
@@ -136,7 +134,7 @@ export class SignupComponent implements OnInit, OnDestroy {
 
   //Función para validar una CURP
   curpValida(curp: any) {
-    console.log(this.signupForm.curp);
+    //console.log(this.signupForm.curp.value);
     console.log(curp);
     var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
       validado = curp.match(re);
