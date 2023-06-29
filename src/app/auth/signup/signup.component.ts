f9702f74233734335c87c21d1d06913607d79288
= <=>
2023-06-29 12:07:23 -0600

    modificando el signunp

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -19,6 +19,7 @@ const log = new Logger('Signup');
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
+
 export class SignupComponent implements OnInit, OnDestroy {
   version: string | null = environment.version;
   error: string | undefined;
@@ -26,6 +27,12 @@ export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
 
   institucionesCatalogo = InstitucionesCatalogo;
+
+  //console
+  console = console;
+
+  console.log("1ss");
+
 
   constructor(
     private router: Router,
@@ -40,9 +47,9 @@ export class SignupComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  ngOnDestroy() {}
+  ngOnDestroy() { }
 
   signup() {
     this.isLoading = true;
@@ -88,6 +95,9 @@ export class SignupComponent implements OnInit, OnDestroy {
     });
   }
 
+  console.log("2ss");
+
+
   private createForm() {
     this.signupForm = this.formBuilder.group({
       nombre: ['', Validators.required],
@@ -103,7 +113,8 @@ export class SignupComponent implements OnInit, OnDestroy {
         ],
       ],
       curp: [
-        '',
+        // '',
+        this.curpValida,
         [
           Validators.required,
           Validators.pattern(
@@ -125,4 +136,55 @@ export class SignupComponent implements OnInit, OnDestroy {
       institucion: [{ disabled: true, value: null }, [Validators.required]],
     });
   }
+
+  console.log("ss");
+
+  //Función para validar una CURP
+  curpValida(curp: any) {
+    var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
+      validado = curp.match(re);
+
+    if (!validado)  //Coincide con el formato general?
+      return false;
+
+    //Validar que coincida el dígito verificador
+    function digitoVerificador(curp17: any) {
+      //Fuente https://consultas.curp.gob.mx/CurpSP/
+      var diccionario = "0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ",
+        lngSuma = 0.0,
+        lngDigito = 0.0;
+      for (var i = 0; i < 17; i++)
+        lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i);
+      lngDigito = 10 - lngSuma % 10;
+      if (lngDigito == 10) return 0;
+      return lngDigito;
+    }
+
+    if (validado[2] != digitoVerificador(validado[1]))
+      return false;
+
+    return true; //Validado
+  }
+  
+  
+  //Handler para el evento cuando cambia el input
+  //Lleva la CURP a mayúsculas para validarlo
+  validarInput(input: any) {
+    var curp = input.value.toUpperCase(),
+      resultado = document.getElementById("resultado"),
+      valido = "No válido";
+
+    if (this.curpValida(curp)) { // ⬅️ Acá se comprueba
+      valido = "Válido";
+      resultado.classList.add("ok");
+    } else {
+      resultado.classList.remove("ok");
+    }
+
+    resultado.innerText = "CURP: " + curp + "\nFormato: " + valido;
+  }
+
 }
+
+console.log("xx");
+console.log("proa");
