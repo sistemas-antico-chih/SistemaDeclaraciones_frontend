895b6a6d8eefdc585a5406304b4787461706f770
= <=>
2023-06-29 12:58:14 -0600

    modificando el signunp

--- src/app/auth/signup/signup.validador.ts
+++ src/app/auth/signup/signup.validador.ts
@@ -1,8 +1,9 @@
 import { FormControl } from '@angular/forms';
 export class curpValidator {
-  static validarCURP(curp: FormControl.curp){
+  static validarCURP(fc: FormControl){
+      
     var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
-    validado = curp.match(re);
+    validado = fc.curp.match(re);
 
   if (!validado)  //Coincide con el formato general?
     return false;
