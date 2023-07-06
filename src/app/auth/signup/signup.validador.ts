f11b582855e8d8ea4445afe98c9bcc5183d847cb
= <=>
2023-07-06 15:03:07 -0600

    modificando signup.validator

--- src/app/auth/signup/signup.validador.ts
+++ src/app/auth/signup/signup.validador.ts
@@ -1,19 +1,13 @@
 import { AbstractControl, ValidatorFn, FormControl } from '@angular/forms';
 
-export function validarCURP(control: FormControl) { (1)
-    let email = control.value; (2)
-    if (email && email.indexOf("@") != -1) { (3)
-      let [_, domain] = email.split("@"); (4)
-      if (domain !== "codecraft.tv") { (5)
-        return {
-          emailDomain: {
-            parsedDomain: domain
-          }
-        }
-      }
+export function validarCURP(control: FormControl) {
+    console.log(control.value);
+    let curp = control.value;
+    if (curp = "JUAS820710HCHRRL02") {
+        return "curpValido";
     }
-    return null; (6)
-  }
+    return null;
+}
 /*export function ageRangeValidator(min: number, max: number): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
         if (control.value !== undefined && (isNaN(control.value) || control.value < min || control.value > max)) {
@@ -27,10 +21,10 @@ export function validarCURP(control: FormControl) { (1)
 /*function curpValida(curp) {
     var re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/,
         validado = curp.match(re);
-	
+
     if (!validado)  //Coincide con el formato general?
-    	return false;
-    
+        return false;
+
     //Validar que coincida el dígito verificador
     function digitoVerificador(curp17) {
         //Fuente https://consultas.curp.gob.mx/CurpSP/
@@ -43,10 +37,10 @@ export function validarCURP(control: FormControl) { (1)
         if (lngDigito == 10) return 0;
         return lngDigito;
     }
-  
-    if (validado[2] != digitoVerificador(validado[1])) 
-    	return false;
-        
+
+    if (validado[2] != digitoVerificador(validado[1]))
+        return false;
+
     return true; //Validado
 }
 
@@ -57,13 +51,13 @@ function validarInput(input) {
     var curp = input.value.toUpperCase(),
         resultado = document.getElementById("resultado"),
         valido = "No válido";
-        
+
     if (curpValida(curp)) { // ⬅️ Acá se comprueba
-    	valido = "Válido";
+        valido = "Válido";
         resultado.classList.add("ok");
     } else {
-    	resultado.classList.remove("ok");
+        resultado.classList.remove("ok");
     }
-        
+
     resultado.innerText = "CURP: " + curp + "\nFormato: " + valido;
 }*/
