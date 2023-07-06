4f688d44476860754730e859536c4ed305eff532
= <=>
2023-07-06 13:10:06 -0600

    modificando signup.validator

--- src/app/auth/signup/signup.validador.ts
+++ src/app/auth/signup/signup.validador.ts
@@ -11,11 +11,11 @@ export function validarCURP(curp_recibida: string): ValidatorFn {
         return null;
     };
 }
-export function ageRangeValidator(min: number, max: number): ValidatorFn {
+/*export function ageRangeValidator(min: number, max: number): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
         if (control.value !== undefined && (isNaN(control.value) || control.value < min || control.value > max)) {
             return { 'ageRange': true };
         }
         return null;
     };
-}
+}*/
