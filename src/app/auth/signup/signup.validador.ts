0ef23c84f030bba0fbfeb91ee76ad903cbb50961
= <=>
2023-07-06 13:35:07 -0600

    modificando signup.validator

--- src/app/auth/signup/signup.validador.ts
+++ src/app/auth/signup/signup.validador.ts
@@ -1,21 +1,18 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function validarCURP(curp_recibida: string): ValidatorFn {
-    return (curp_recibida: AbstractControl): { [key: string]: boolean } | null => {
-        
-
-        
-        if (curp_recibida="JUAS820710HCHRRL02") {
+export function validarCURP(curp_recibida: any): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+        if (control.value="JUAS820710HCHRRL02") {
             return { 'curpValida': true };
         }
         return null;
     };
 }
-/*export function ageRangeValidator(min: number, max: number): ValidatorFn {
+export function ageRangeValidator(min: number, max: number): ValidatorFn {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
         if (control.value !== undefined && (isNaN(control.value) || control.value < min || control.value > max)) {
             return { 'ageRange': true };
         }
         return null;
     };
-}*/
+}
