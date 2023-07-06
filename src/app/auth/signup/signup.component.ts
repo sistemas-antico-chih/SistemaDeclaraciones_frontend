9170eb3262428cda9be7b8420fbf10e68ab0337f
= <=>
2023-07-06 12:01:44 -0600

    modificando signup.validator

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -119,7 +119,7 @@ export class SignupComponent implements OnInit, OnDestroy {
       curp: [
          '',
         [
-          validadores.validarCURP,
+          //validadores.validarCURP,
           Validators.required,
           Validators.pattern(
             /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/i
