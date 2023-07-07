1a912943a6ee7fd20fc62bbfa5a70efa43408ca7
= <=>
2023-07-07 14:58:17 -0600

    modificando sigun.component.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -11,6 +11,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 import InstitucionesCatalogo from '@static/custom/instituciones.json';
 
+import { validarCURP } from '../signup/signup.validador';
+
 const log = new Logger('Signup');
 
 @UntilDestroy()
@@ -106,11 +108,12 @@ export class SignupComponent implements OnInit, OnDestroy {
         '',
         [
           Validators.required,
-          Validators.pattern(
+          validarCURP
+          /*Validators.pattern(
             /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/i
-          ),
+          ),*/
         ],
-      ],
+      ],updatedOn: 'change',
       rfc: [
         '',
         [
