ace7b85dc18b16c08c482daf7c5b47e9bc5455d5
= <=>
2023-07-11 15:30:28 -0600

    realizar modal

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
@@ -1,49 +1,10 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import {MatDialogModule , MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
-
-export interface DialogData {
-  animal: string;
-  name: string;
-}
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comienza-tu-declaracion',
   templateUrl: './comienza-tu-declaracion.component.html',
   styleUrls: ['./comienza-tu-declaracion.component.scss'],
 })
-
-
-
-export class DialogOverviewExample {
-  animal: string;
-  name: string;
-
-  constructor(public dialog: MatDialogModule ) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: {name: this.name, animal: this.animal},
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
-}
-
-export class DialogOverviewExampleDialog {
-  constructor(
-    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,
-  ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-}
-
 export class ComienzaTuDeclaracionComponent implements OnInit {
   constructor() {}
 
--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.module.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {NgIf} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { CoreModule } from '@core';
 import { SharedModule } from '@shared';
-//import { MatDialogModule  } from '@app/material.module';
+import { MaterialModule } from '@app/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { ComienzaTuDeclaracionRoutingModule } from './comienza-tu-declaracion-routing';
@@ -18,16 +12,11 @@ import { ComienzaTuDeclaracionComponent } from './comienza-tu-declaracion.compon
 @NgModule({
   declarations: [ComienzaTuDeclaracionComponent],
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatButtonModule,
-    NgIf,
-    MatDialogModule,
     CommonModule,
     CoreModule,
     SharedModule,
     FlexLayoutModule,
+    MaterialModule,
     ComienzaTuDeclaracionRoutingModule,
   ],
 })
