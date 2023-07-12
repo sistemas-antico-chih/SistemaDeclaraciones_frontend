92fb10648d78f030eb95eba2d5e4d53e904073b8
= <=>
2023-07-12 12:18:20 -0600

    agregar dialog / modal

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+interface City {
+  name: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-comienza-tu-declaracion',
   templateUrl: './comienza-tu-declaracion.component.html',
   styleUrls: ['./comienza-tu-declaracion.component.scss'],
 })
-export class ComienzaTuDeclaracionComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class ComienzaTuDeclaracionComponent implements OnInit {
+  //constructor() {}
+
+  //ngOnInit(): void {}
+  cities: City[] | undefined;
+
+    selectedCity: City | undefined;
+
+    visible: boolean = false;
+
+    ngOnInit() { 
+        this.cities = [
+            {name: 'New York', code: 'NY'},
+            {name: 'Rome', code: 'RM'},
+            {name: 'London', code: 'LDN'},
+            {name: 'Istanbul', code: 'IST'},
+            {name: 'Paris', code: 'PRS'}
+        ];
+    }
+
+    showDialog() {
+        this.visible = true;
+    }
 }
