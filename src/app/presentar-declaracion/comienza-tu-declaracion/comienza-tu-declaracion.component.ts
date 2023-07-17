24a7e67f033804e7e109e30bb7eb5d7bbdeb06db
= <=>
2023-07-17 14:14:39 -0600

    probando ruta de dialog

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
@@ -29,7 +29,7 @@ export class ComienzaTuDeclaracionComponent  {
       closeOnNavigation: true,
     });
     console.log("component has been initialized! openDialog");
-    this.dialog
+    this.dialog.open()
     .subscribe(()=>{
       dialogRef.close();
     })
