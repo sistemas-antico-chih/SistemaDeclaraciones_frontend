9403c8ce1974283ddf52d5dfe721c5ef18df9d57
= <=>
2023-07-18 10:45:56 -0600

    probando ruta de dialog

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material/dialog';
 //import { ComienzaTuDeclaracionComponent } from '../comienza-tu-declaracion/comienza-tu-declaracion.component'
 
 @Component({
