fdbc416b776f318c86b4035e623ee34352844cc7
= <=>
2023-07-19 13:17:02 -0600

    modificando dialog para poner formulario

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
@@ -4,8 +4,8 @@ import { Router } from '@angular/router';
 //import { Catalogo, DatosDialog } from '@models/declaracion';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-
-
+import { tooltipData } from '@static/tooltips/situacion-patrimonial/datos-empleo';
+import { DeclarationErrorStateMatcher } from '@app/presentar-declaracion/shared-presentar-declaracion/declaration-error-state-matcher';
 
 import catalogoPuestos from '@static/catalogos/catalogoPuestos.json';
 import { DatosDialog } from '@models/declaracion/datos-dialog.model';
@@ -18,7 +18,16 @@ import { DatosDialog } from '@models/declaracion/datos-dialog.model';
 
 export class DialogElementsExampleDialog implements OnInit{
   catalogoPuestos=catalogoPuestos;
-  datosComponenteForm: FormGroup;
+  datosDialogForm: FormGroup;
+
+  tooltipData = tooltipData;
+  errorMatcher = new DeclarationErrorStateMatcher();
+
+  minDate = new Date(1980, 1, 1);
+  anio: number = new Date().getFullYear();
+  mes: number = new Date().getMonth() + 1;
+  dia: number = new Date().getDate();
+  maxDate = new Date(this.anio, this.mes, this.dia);
 
   //constructor(public dialog: MatDialog) {}
   constructor(
@@ -39,13 +48,16 @@ export class DialogElementsExampleDialog implements OnInit{
   }
 
   createForm() {
-    this.datosComponenteForm = this.formBuilder.group({
+    this.datosDialogForm = this.formBuilder.group({
     })
     //catalogoPuestos: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
   }
 
   fillForm(datosComponenteForm: DatosDialog | undefined) {
-    this.datosComponenteForm.patchValue(datosComponenteForm || {});
+    this.datosDialogForm.patchValue(datosComponenteForm || {});
 
+  }
+  confirmSaveInfo() {
+    console.log("boton guardar");
   }
 }
