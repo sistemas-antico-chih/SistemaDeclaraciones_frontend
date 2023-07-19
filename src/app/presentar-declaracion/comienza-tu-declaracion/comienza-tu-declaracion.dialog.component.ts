0ef84e204c5acc2d352501673acca76ae3e4e2cc
= <=>
2023-07-19 10:17:29 -0600

    modificando dialog para poner select

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { datosEmpleoCargoComisionQuery, declaracionMutation } from '@api/declaracion';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+
+
 import catalogoPuestos from '@static/catalogos/catalogoPuestos.json';
 
 @Component({
   selector: 'comienza-tu-declaracion.dialog',
   templateUrl: './comienza-tu-declaracion.dialog.component.html',
-  //styleUrls: ['./comienza-tu-declaracion.component.scss'],
+  styleUrls: ['./comienza-tu-declaracion.component.scss'],
 })
 
 export class DialogElementsExampleDialog implements OnInit{
@@ -15,12 +21,18 @@ export class DialogElementsExampleDialog implements OnInit{
   constructor(
     private dialogRef: MatDialogRef<DialogElementsExampleDialog>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {}
+  ) {
+    this.createForm();
+  }
   
   ngOnInit(){
 
   }
   closeDialog() {
     this.dialogRef.close({ data: '' })
+  }
+
+  createForm() {
+    //catalogoPuestos: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
   }
 }
