bdc85b587c878a6039b132e6b84da50c61417018
= <=>
2023-09-11 09:56:13 -0600

    modificar comienza declaraciones para agregar ruta que compruebe declaraciones iniciales y conclusion

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.dialog.component.ts
@@ -229,12 +229,13 @@ export class DialogElementsExampleDialog implements OnInit {
       switch (tipo) {
         case "inicial":
           const dialogRefInicial = this.dialog.open(DialogComponent, {
+            height: '40px',
+            width: '90px',
             data: {
               title: 'No es posible iniciar la declaración de tipo INICIAL',
               message: 'Es neceario que exista la declaración firmada de tipo CONCLUSIÓN correspondiente, en caso de alguna duda favor de dirigirse a su Órano Interno de Control.',
               trueText: 'Continuar',
-              height: '40px',
-              width: '90px',            },
+            },
           });
           break;
         case "modificacion":
