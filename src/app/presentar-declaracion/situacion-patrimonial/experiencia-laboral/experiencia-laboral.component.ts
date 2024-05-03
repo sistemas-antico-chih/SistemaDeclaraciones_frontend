28b1d194131d480529a5828e3463bb9f7ddd81e7
= <=>
2024-05-03 11:23:16 -0600

    agregando validacion a fechas en experiencia Laboral

--- src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.component.ts
@@ -12,7 +12,7 @@ import { experienciaLaboralMutation, experienciaLaboralQuery } from '@api/declar
 import { DeclarationErrorStateMatcher } from '@app/presentar-declaracion/shared-presentar-declaracion/declaration-error-state-matcher';
 import { UntilDestroy, untilDestroyed } from '@core';
 
-import { validarFechas } from '../experiencia-laboral/experiencia-laboral.validador';
+import { validarFechaInicial, validarFechas } from '../experiencia-laboral/experiencia-laboral.validador';
 import { DeclaracionOutput } from '@models/declaracion/declaracion.model';
 import { Experiencia, ExperienciaLaboral } from '@models/declaracion/experiencia-laboral.model';
 import AmbitoPublico from '@static/catalogos/ambitoPublico.json';
@@ -152,7 +152,7 @@ export class ExperienciaLaboralComponent implements OnInit {
         areaAdscripcion: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
         empleoCargoComision: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
         funcionPrincipal: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
-        fechaIngreso: [null, [Validators.required]],
+        fechaIngreso: [null, [Validators.required, validarFechaInicial]],
         fechaEgreso: [null, [Validators.required, validarFechas]],
         ubicacion: [null, [Validators.required]],
         nombreEmpresaSociedadAsociacion: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
--- src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.validador.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.validador.ts
@@ -1,28 +1,35 @@
 import { AbstractControl, ValidatorFn, FormControl } from '@angular/forms';
 
-export function validarFechas(control: FormControl) {
-    console.log("control: "+control.value);
-    
-    let fechaInicial = this.experienciaLaboralForm.fechaIngreso;
-    let fechaFinal = control.value.toUpperCase();
-    console.log("fechaInicial: "+fechaInicial);
-    
-    if (!fechaInicial || !fechaFinal )
+export function validarFechaInicial(control: FormControl) {
+    var fechaIncial = control.value;
+    console.log("control 1: " + control.value);
+    if (!fechaIncial)
         return false;
-    
-    
-    
+}
+
+export function validarFechas(control: FormControl) {
+    console.log("control 2: " + control.value);
+
+
+    let fechaFinal = control.value;
+    console.log("fechaInicial: " + this.fechaInicial);
+
+    if ( !fechaFinal)
+        return false;
+
+
+
 
     //Validar que coincida el dígito verificador
-    function comparar( fechaInicial:any, fechaFinal:any) {
+    function comparar(fechaInicial: any, fechaFinal: any) {
         //Fuente https://consultas.curp.gob.mx/CurpSP/
-        if(Date.parse(fechaInicial) < Date.parse(fechaFinal)){
+        if (Date.parse(fechaInicial) < Date.parse(fechaFinal)) {
             return true;
         }
         else {
             return false;
         }
-        
+
     }
 
     if (comparar)
