68f7f1214d86e1f4d7ad1afb3353be64b0995fb4
= <=>
2024-05-06 12:24:07 -0600

    agregando validacion a fechas en experiencia Laboral

--- src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.validador.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/experiencia-laboral/experiencia-laboral.validador.ts
@@ -1,19 +1,22 @@
-import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { FormGroup, ValidationErrors, ValidatorFn, Date } from '@angular/forms';
 
 
 export const validarFechas: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
     console.log("llega¡¡");
-    let fechaIni = control.get('experiencia.fechaIngreso');
-    let fechaFin = control.get('experiencia.fechaEgreso');
+    let fechaInicial = control.get('experiencia.fechaIngreso').split("/");
+    let fechaFinal = control.get('experiencia.fechaEgreso').split("/");
   
-    console.log("fechaInicial: "+fechaIni);
-    console.log("fechaFinal: "+fechaFin);
+    console.log("fechaInicial: "+fechaInicial);
+    console.log("fechaFinal: "+fechaFinal);
 
-    let fechaInicial= new Date(fechaIni)
-    let fechaFinal=new Date(fechaFin)
+    console.log("fechaInicial 1: "+fechaInicial[1]);
+    console.log("fechaFinal 1: "+fechaFinal[1]);
 
-    console.log("fechaInicial 2: "+fechaInicial);
-    console.log("fechaFinal 2: "+fechaFinal);
+    console.log("fechaInicial 2: "+fechaInicial[2]);
+    console.log("fechaFinal 2: "+fechaFinal[2]);
 
-    return fechaInicial < fechaFinal ? null : { 'fechaErronea': true };
+    console.log("fechaInicial 3: "+fechaInicial[3]);
+    console.log("fechaFinal 3: "+fechaFinal[3]);
+
+    return fechaInicial[1] < fechaFinal[1] ? null : { 'fechaErronea': true };
   };
