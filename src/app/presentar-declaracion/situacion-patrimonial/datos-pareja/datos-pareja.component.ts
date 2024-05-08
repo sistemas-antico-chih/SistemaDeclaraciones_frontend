101ccb892c65d6f3fc5f22746823c2e8c6c974b0
= <=>
2024-05-08 08:51:28 -0600

    ocultar rfc / curp cuando corresponda en datosPareja

--- src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { Apollo } from 'apollo-angular';
@@ -24,7 +24,6 @@ import RelacionConDeclarante from '@static/catalogos/relacionConDeclarante.json'
 import Sector from '@static/catalogos/sector.json';
 import { tooltipData } from '@static/tooltips/situacion-patrimonial/datos-pareja';
 import { findOption } from '@utils/utils';
-import TipoOperacion from '@static/catalogos/tipoOperacion.json';
 
 @UntilDestroy()
 @Component({
@@ -53,7 +52,7 @@ export class DatosParejaComponent implements OnInit {
   monedasCatalogo = Monedas;
   municipiosCatalogo = Municipios;
   paisesCatalogo = Paises;
-  tipoOperacionCatalogo = TipoOperacion;
+
   pareja: DatosPareja = null;
 
   tipoDeclaracion: string = null;
@@ -63,17 +62,6 @@ export class DatosParejaComponent implements OnInit {
 
   tooltipData = tooltipData;
   errorMatcher = new DeclarationErrorStateMatcher();
-
-  minDatePareja = new Date(1940, 1, 1);
-  minDate = new Date(1960, 1, 1);
-  anio: number = new Date().getFullYear();
-  mes: number = new Date().getMonth();
-  dia: number = new Date().getDate();
-  maxDate = new Date(this.anio, this.mes-1, this.dia);
-  hidden: string = 'false';
-  
-  ciudadanoExtranjero: boolean;
-  active: boolean;
 
   constructor(
     private apollo: Apollo,
@@ -128,7 +116,6 @@ export class DatosParejaComponent implements OnInit {
 
   createForm() {
     this.datosParejaForm = this.formBuilder.group({
-      tipoOperacion: [null, [Validators.required]],
       ninguno: [false],
       nombre: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
       primerApellido: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
@@ -143,7 +130,7 @@ export class DatosParejaComponent implements OnInit {
         ],
       ],
       relacionConDeclarante: [null, [Validators.required]],
-      ciudadanoExtranjero: [null, [Validators.required]],
+      ciudadanoExtranjero: [false, [Validators.required]],
       curp: [
         null,
         [
@@ -197,7 +184,7 @@ export class DatosParejaComponent implements OnInit {
           { disabled: true, value: null },
           [
             Validators.pattern(
-              /^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/i
+              /^([A-ZÑ&]{3}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/i
             ),
           ],
         ],
@@ -250,36 +237,6 @@ export class DatosParejaComponent implements OnInit {
       this.lugarDondeResideChanged(value);
     });
   }
-
-  radioChange(event:any) {
-    this.hidden = event;
-    this.active = this.datosParejaForm.controls['ciudadanoExtranjero'].value;
-    console.log(this.active)
-    if (this.active == false) {
-      console.log("llega extranjero!!");
-      console.log(this.active)
-      this.datosParejaForm.get("rfc").setValidators([Validators.required]);
-      this.datosParejaForm.get("rfc").enable();
-      this.datosParejaForm.get("rfc").updateValueAndValidity();
-      this.datosParejaForm.get("curp").setValidators([Validators.required]);
-      this.datosParejaForm.get("curp").enable();
-      this.datosParejaForm.get("curp").updateValueAndValidity();
-    } else {
-      console.log("llega TRUE!!");
-      console.log(this.active)
-      this.datosParejaForm.get("rfc").clearValidators();
-      this.datosParejaForm.get("rfc").updateValueAndValidity();
-      this.datosParejaForm.get("rfc").disable();
-      
-      this.datosParejaForm.get("curp").clearValidators();
-      this.datosParejaForm.get("curp").updateValueAndValidity();
-      this.datosParejaForm.get("curp").disable();
-      
-      //rfc: new FormControl({value: '', disabled:true})
-    }
-    console.log("Requerido", this.datosParejaForm.errors);
-  }
-  
 
   fillForm(datosPareja: DatosPareja) {
     this.datosParejaForm.patchValue(datosPareja || {});
