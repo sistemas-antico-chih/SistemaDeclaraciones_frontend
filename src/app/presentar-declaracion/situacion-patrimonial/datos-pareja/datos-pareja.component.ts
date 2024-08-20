69e379c665792e703b6ad14cdce3af7ff9e849ea
= <=>
2024-08-20 10:10:10 -0600

    agregar botón para guardar cambios en datosPareja / ninguno -se agrega 'tipoOperacion'

--- src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
@@ -429,11 +429,14 @@ export class DatosParejaComponent implements OnInit {
 
   checkPartner(){
     const form = JSON.parse(JSON.stringify(this.datosParejaForm.value)); // Deep copy
-    this.datosParejaForm.value.get('tipoOperacion').setValue('SIN_CAMBIOS')
+    console.log("llega 1");
+    const tipo_operacion=this.datosParejaForm.get('tipoOperacion').setValue('SIN_CAMBIOS')
+    console.log("llega 2");
+    console.log(tipo_operacion)
       if (form.nombre !== null){
         const aclaracionesObservaciones = this.datosParejaForm.value.aclaracionesObservaciones;
         this.isLoading = true;
-        this.saveInfo(this.finalForm);
+        this.saveInfo(this.datosParejaForm);
         this.saveInfo(aclaracionesObservaciones);
         this.isLoading = false;
       }else{
