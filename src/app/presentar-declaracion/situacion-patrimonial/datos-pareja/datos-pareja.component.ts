226bacff8fd04541c96d36105d74e6bd1cfecf9c
= <=>
2024-08-23 08:56:57 -0600

    corregir funcion que guarda en datosPareja

--- src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
@@ -63,6 +63,14 @@ export class DatosParejaComponent implements OnInit {
   tooltipData = tooltipData;
   errorMatcher = new DeclarationErrorStateMatcher();
 
+  minDatePareja = new Date(1940, 1, 1);
+  minDate = new Date(1960, 1, 1);
+  anio: number = new Date().getFullYear();
+  mes: number = new Date().getMonth();
+  dia: number = new Date().getDate();
+  maxDate = new Date(this.anio, this.mes - 1, this.dia);
+  //hidden: string = 'false';
+
   constructor(
     private apollo: Apollo,
     private dialog: MatDialog,
