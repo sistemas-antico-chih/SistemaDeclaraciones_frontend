beb84f45b5f603a5fef30f1aeadeea10380dfa14
= <=>
2024-10-11 15:11:12 -0600

    corregir OTRO_ESPECIFIQUE de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -489,7 +489,7 @@ export class IngresosNetosComponent implements OnInit {
   }
 
   selectChange(event: any) {
-    this.otroTipoInstrumento=event;
+    this.tipoInstrumento=event;
     console.log(event);
     /*this.hidden = event;
     this.active = this.datosParejaForm.controls['ciudadanoExtranjero'].value;
