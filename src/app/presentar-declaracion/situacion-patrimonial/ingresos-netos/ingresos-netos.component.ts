fc42f4d545e1845153ed8aea00b3aff2f8eac335
= <=>
2024-10-17 10:23:10 -0600

    corregir OTRO_ESPECIFIQUE de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -32,7 +32,7 @@ import { findOption } from '@utils/utils';
 export class IngresosNetosComponent implements OnInit {
   
   //@ViewChildren('otroTipoInstrumento') otroTipoInstrumento!: QueryList<ElementRef>;
-  @ViewChild('otroTipoInstrumento') otroTipoInstrumento:ElementRef;
+  @ViewChild('otroTipoInstrumento[i]') otroTipoInstrumento[i]:ElementRef;
   
   isHidden = true;
 
