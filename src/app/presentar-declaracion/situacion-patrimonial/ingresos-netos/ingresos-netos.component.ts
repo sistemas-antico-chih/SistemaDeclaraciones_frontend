d2b4bf1698805efffeae6011fe18258525c2f136
= <=>
2024-10-22 15:03:55 -0600

    corregir OTRO_ESPECIFIQUE de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -461,8 +461,6 @@ export class IngresosNetosComponent implements OnInit {
     //console.log(form.actividadFinanciera.actividades.length);
 
     let arreglo = this.otroTipoInstrumento.toArray();
-    let prueba = this.otroTipoInstrumento.nativeElement as HTMLInputElement;
-    console.log("prueba: "+prueba);
 
     console.log(arreglo[0]);
     console.log("******");
