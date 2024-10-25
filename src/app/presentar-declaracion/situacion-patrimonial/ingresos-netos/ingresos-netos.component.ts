c245ac301cddca49d719140ccb06ce0320a61d8d
= <=>
2024-10-25 10:04:00 -0600

    corregir funcion fill de 'OTRO_ESPECIFIQUE' de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -322,13 +322,19 @@ export class IngresosNetosComponent implements OnInit {
           .setValue(findOption(this.tipoInstrumentoCatalogo, tipoInstrumento?.clave));
 
         let algo;
+        const myElement: HTMLElement | null = document.getElementById('mat-input-20');
+        algo=myElement;
+        console.log(algo);
         if(tipoInstrumento.clave === 'OTRO'){
           console.log("1");
-          (<HTMLInputElement>document.getElementById("mat-input-20")).value="lguillo";
-          algo=(<HTMLInputElement>document.getElementById("mat-input-20")).value
-          console.log("2");
+          //(<HTMLInputElement>document.getElementById("mat-input-20")).value="lguillo";
+          //algo= (document.getElementById("mat-input-20") as HTMLInputElement).value;
+          //algo=(<HTMLInputElement>document.getElementById("mat-input-20")).value
+          
+          
+          //console.log("2");
           //console.log((<HTMLInputElement>document.getElementById("mat-input-20")).value);
-          console.log("3");
+          //console.log("3");
           //const user: string = document.querySelector<HTMLInputElement>('input[id="mat-input-20"]').value;
           //console.log(user);
           //console.log(arreglo);
