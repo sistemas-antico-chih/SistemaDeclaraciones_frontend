8237905af595c5b5c355535f72fe8e7c26dfa010
= <=>
2024-10-29 10:25:12 -0600

    corregir funcion fill de 'OTRO_ESPECIFIQUE' de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -368,37 +368,19 @@ export class IngresosNetosComponent implements OnInit {
     }
     
   }
-  /*
+  
   ngAfterContentChecked(): void {
     console.log("ngAfterContentChecked")
     const form = JSON.parse(JSON.stringify(this.ingresosForm.value)); // Deep copy
-    console.log("final 1")
+    console.log(this.otroTipoInstrumento);
     
     //let arreglo = this.otroTipoInstrumento.toArray();
     console.log(this.ingresosForm.get('actividadFinanciera.actividades'));
-    if (this.otroTipoInstrumento !== undefined || this.otroTipoInstrumento !=null){
+    /*if (this.otroTipoInstrumento !== undefined || this.otroTipoInstrumento !=null){
       console.log(this.otroTipoInstrumento)
       //const { tipoInstrumento } = formArray.at(index).value;
-    }
-    
-    let x=0;
-    for (let j = 0; j < form.actividadFinanciera.actividades.length; j++) {
-      if (form.actividadFinanciera.actividades[j].tipoInstrumento.clave === "OTRO") {
-        //obValores.push(form.actividadFinanciera.actividades[j].tipoInstrumento.valor);
-        this.otroTipoInstrumento.forEach(function (value: any) {
-          if (value !== undefined) {
-            
-            console.log(x);
-            x++;
-          //  (<HTMLInputElement> document.getElementById("mat-input-20")).value="alguito"
-            //obValores = arreglo[j].nativeElement.id;
-            //valorHtml = document.getElementById(obValores) as HTMLInputElement;
-            //form.actividadFinanciera.actividades[j].tipoInstrumento.valor = valorHtml.value;
-          }
-        });
-      }
-    }
-  }*/
+    }*/
+  }
   
   fillForm(ingresos: Ingresos) {
     this.ingresosForm.patchValue(ingresos);
