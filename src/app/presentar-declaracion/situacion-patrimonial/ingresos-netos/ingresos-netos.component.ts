d1d6abb4c24ae5e8b1dcd02c1121d229f419a697
= <=>
2024-10-31 12:34:23 -0600

    corregir funcion fill de 'OTRO_ESPECIFIQUE' de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -449,8 +449,8 @@ export class IngresosNetosComponent implements OnInit {
       },
     });
 
-    let test = document.querySelectorAll<HTMLInputElement>('.OTI')[0].id;
-    console.log("test 1: " + test);  
+    //let test = document.querySelectorAll<HTMLInputElement>('.OTI')[0].id;
+    console.log("test 1: " + document.querySelectorAll<HTMLInputElement>('.OTI')[0].id);  
   }
 
   ngAfterViewInit() {
@@ -458,8 +458,8 @@ export class IngresosNetosComponent implements OnInit {
       this.arrayHTMLOtroTipoInstrumento.push(document.querySelectorAll<HTMLInputElement>('.OTI')[j])
       document.querySelectorAll<HTMLInputElement>('.OTI')[j].value=this.arrayOtroTipoInstrumento[j];
     }
-    let test = document.querySelectorAll<HTMLInputElement>('.OTI')[0].id;
-    console.log("test 2: " + test);
+    //let test = document.querySelectorAll<HTMLInputElement>('.OTI')[0].id;
+    console.log("test 2: " + document.querySelectorAll<HTMLInputElement>('.OTI')[0].id);
   }
 
 
