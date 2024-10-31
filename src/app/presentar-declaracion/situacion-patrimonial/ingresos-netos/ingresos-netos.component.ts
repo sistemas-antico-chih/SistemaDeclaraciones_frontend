d6a4aeac43dd8e46bc56d12b287c56cce807dbba
= <=>
2024-10-30 18:37:49 -0600

    corregir funcion fill de 'OTRO_ESPECIFIQUE' de ingresos.tipoInstrumento

--- src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/ingresos-netos/ingresos-netos.component.ts
@@ -469,9 +469,9 @@ export class IngresosNetosComponent implements OnInit {
 
     let test = document.querySelectorAll<HTMLInputElement>('.OTI')[0].id;
     let large = document.querySelectorAll<HTMLInputElement>('.OTI').length;
-    console.log("ngOnInit");
-    console.log("test 1: " + test);
-    console.log("large 1: " + large)
+    //console.log("ngOnInit");
+    //console.log("test 1: " + test);
+    //console.log("large 1: " + large)
   
   }
 
