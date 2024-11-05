b83fbdca84cc9efa8cc14fd800d46f0665d62232
= <=>
2024-11-05 12:18:09 -0600

    corregir funcion fill de 'OTRO_ESPECIFIQUE' de bienesInmuebles.tipoInmueble y bienesInmuebles.relacion

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -45,7 +45,7 @@ export class BienesInmueblesComponent implements OnInit {
   bienInmueble: BienInmueble[] = [];
   isLoading = false;
 
-  @ViewChild('otroTipoInmueble') otroTipoInmueblee: ElementRef;
+  @ViewChild('otroTipoInmueble') otroTipoInmueble: ElementRef;
   @ViewChild('otroParentesco') otroParentesco: ElementRef;
 
   tipoInmuebleCatalogo = TipoInmueble;
@@ -235,10 +235,9 @@ export class BienesInmueblesComponent implements OnInit {
     }
 
     if (bienInmueble.tipoInmueble?.clave === 'OTRO') {
-      console.log("llega")
-      console.log(bienInmueble.tipoInmueble.valor);
-      this.otroTipoInmueblee.nativeElement.value = bienInmueble.tipoInmueble?.valor;
-      //this.otroTipoInmueble.nativeElement.value = "alguillo"
+      console.log("llega IF")
+      //this.otroTipoInmueble.nativeElement.value = bienInmueble.tipoInmueble?.valor;
+      this.otroTipoInmueble.nativeElement.value = "alguillo"
     }
 
     /*if (bienInmueble.transmisor[0].relacion?.clave === 'OTRO') {
@@ -459,7 +458,7 @@ export class BienesInmueblesComponent implements OnInit {
     const form = JSON.parse(JSON.stringify(this.bienesInmueblesForm.value.bienInmueble)); // Deep copy
 
     if (form.tipoInmueble?.clave === 'OTRO') {
-      //form.tipoInmueble.valor = this.otroTipoInmueble.nativeElement.value;
+      form.tipoInmueble.valor = this.otroTipoInmueble.nativeElement.value;
     }
     if (form.transmisor.relacion?.clave === 'OTRO') {
       form.transmisor.relacion.valor = this.otroParentesco.nativeElement.value;
