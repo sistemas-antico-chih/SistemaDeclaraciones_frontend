c8059672bf0ce5ad677fb767ceae97f5ee1c5611
= <=>
2024-11-14 14:04:31 -0600

    bienesInmuebles.tipoInmueble 'OTRO_ESPECIFIQUE'

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -224,8 +224,11 @@ export class BienesInmueblesComponent implements OnInit {
     console.log("44");
     console.log(this.bienesInmueblesForm.get('bienInmueble.titular'));
     
-    if(this.bienesInmueblesForm.get('bienInmueble.titular')){
-      this.bienesInmueblesForm.get('bienInmueble.titular.valor').setValue(bienInmueble.titular[0].valor)
+    const { tipoInmueble, titular, formaAdquisicion } = this.bienesInmueblesForm.value.bienInmueble;
+
+    if(titular){
+        this.bienesInmueblesForm.get('bienInmueble.titular')
+          .setValue(findOption(this.titularBienCatalogo, titular[0]));
     }
     //this.bienesInmueblesForm.get('bienInmueble.tipoInmueble.clave').setValue('OTRO');
     
