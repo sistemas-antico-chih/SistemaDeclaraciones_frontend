86b6318313997bfb011414807b9d8a9a9ebcacbb
= <=>
2024-11-20 12:49:01 -0600

    OTRO_ESPECIFIQUE en bienesInmuebles

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -46,7 +46,7 @@ export class BienesInmueblesComponent implements OnInit {
   bienInmueble: BienInmueble[] = [];
   isLoading = false;
 
-  @ViewChild('otroTipoInmueble') otroTipoInmueble: ElementRef;
+  @ViewChild('otroTipoInmueble',{static: false}) otroTipoInmueble: ElementRef;
   @ViewChild('otroParentesco') otroParentesco: ElementRef;
 
   //@ViewChildren('otroTipoInmueble') otroTipoInmueble: QueryList<ElementRef>;
@@ -516,12 +516,10 @@ export class BienesInmueblesComponent implements OnInit {
     if (tipoInmueble) {
       const optionTipoInmueble = this.tipoInmuebleCatalogo.filter((i: any) => i.clave === tipoInmueble.clave);
       this.bienesInmueblesForm.get('bienInmueble.tipoInmueble').setValue(optionTipoInmueble[0]);
-      console.log("aqui");
-      console.log(tipoInmueble);
       if(tipoInmueble.clave ==='OTRO'){
         console.log("llega aqui");
         console.log(tipoInmueble)
-        //this.otroTipoInmueble.nativeElement.value=tipoInmueble?.valor;
+        this.otroTipoInmueble.nativeElement.value=tipoInmueble?.valor;
       } 
     }
 
