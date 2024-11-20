27bf9c37ed0cdbaf1d787d7ff68095e87be6f9b2
= <=>
2024-11-20 13:38:19 -0600

    OTRO_ESPECIFIQUE en bienesInmuebles

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -534,7 +534,8 @@ export class BienesInmueblesComponent implements OnInit {
       if(tipoInmueble.clave ==='OTRO'){
         console.log("llega aqui");
         console.log(tipoInmueble)
-        document.querySelector<HTMLInputElement>('.OTI').value="this.arrayOtroTipoInstrumento[j]";
+        //document.querySelector<HTMLInputElement>('.OTI').value="this.arrayOtroTipoInstrumento[j]";
+        document.getElementById<HTMLInputElement>('mat-input-11').value="this.arrayOtroTipoInstrumento[j]";
         //otroTipoInmueble.nativeElement.value=tipoInmueble?.valor;
       } 
     }
