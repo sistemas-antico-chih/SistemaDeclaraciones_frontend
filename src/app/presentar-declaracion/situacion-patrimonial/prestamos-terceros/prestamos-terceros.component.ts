f6b0b7dd5e6734098528e7597685aa3b5b666d59
= <=>
2024-11-27 15:20:48 -0600

    OTRO_ESPECIFIQUE prestamos

--- src/app/presentar-declaracion/situacion-patrimonial/prestamos-terceros/prestamos-terceros.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/prestamos-terceros/prestamos-terceros.component.ts
@@ -232,7 +232,6 @@ export class PrestamosTercerosComponent implements OnInit {
     ifExistsEnableFields(prestamo.tipoBien.inmueble, this.prestamoComodatoForm, 'prestamo.tipoBien.inmueble');
     if (prestamo.tipoBien.inmueble) {
       this.tipoBien = 'inmueble';
-      console.log(prestamo);
 
       Object.keys(prestamo.tipoBien.inmueble)
         .filter((field) => prestamo.tipoBien.inmueble[field] !== null)
@@ -242,7 +241,7 @@ export class PrestamosTercerosComponent implements OnInit {
             .patchValue(prestamo.tipoBien.inmueble[field])
         );
 
-      /* ifExistsEnableFields(
+      ifExistsEnableFields(
         prestamo.tipoBien.inmueble.domicilioMexico,
         this.prestamoComodatoForm,
         'prestamo.tipoBien.inmueble.domicilioMexico'
@@ -258,9 +257,8 @@ export class PrestamosTercerosComponent implements OnInit {
       );
       if (prestamo.tipoBien.inmueble.domicilioExtranjero) {
         this.tipoDomicilio = 'EXTRANJERO';
-      } */
+      }
     }
-    /*
     ifExistsEnableFields(prestamo.tipoBien.vehiculo, this.prestamoComodatoForm, 'prestamo.tipoBien.vehiculo');
     if (prestamo.tipoBien.vehiculo) {
       this.tipoBien = 'vehiculos';
@@ -290,7 +288,7 @@ export class PrestamosTercerosComponent implements OnInit {
         this.tipoDomicilio = 'EXTRANJERO';
       }
     }
-    */
+
     this.setSelectedOptions();
   }
 
