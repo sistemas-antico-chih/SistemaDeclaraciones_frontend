6da399a9610908317b0f93273664f465294208e3
= <=>
2025-03-11 09:49:05 -0600

    vehiculos fijar rango de anio

--- src/app/presentar-declaracion/situacion-patrimonial/vehiculos/vehiculos.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/vehiculos/vehiculos.component.ts
@@ -147,9 +147,9 @@ export class VehiculosComponent implements OnInit {
         //anio: [null, [Validators.required, Validators.pattern(/^\d{4}$/)]],
         anio: [null, [
           Validators.required, 
-          //Validators.min(1920), 
-          //Validators.max(this.maxAnio),
-          Validators.maxLength(4),
+          Validators.min(1920), 
+          Validators.max(this.maxAnio),
+          //Validators.maxLength(4),
           //Validators.pattern(/^\d{4}$/)
         ]],
         numeroSerieRegistro: [null, [Validators.required, Validators.pattern(/^\S.*\S$/)]],
