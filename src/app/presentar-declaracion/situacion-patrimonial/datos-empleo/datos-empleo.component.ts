46a234f3c594bfebf7f92ccd0752b3311c3980f8
= <=>
2025-03-12 13:07:32 -0600

    adecuar ente_publico dependiendo del poder y ambito en datosEmpleo

--- src/app/presentar-declaracion/situacion-patrimonial/datos-empleo/datos-empleo.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-empleo/datos-empleo.component.ts
@@ -335,14 +335,14 @@ export class DatosEmpleoComponent implements OnInit {
   cambioPoder(value: any) {
     console.log("poder");
     console.log(value);
-    console.log(this.ambito);
+    console.log(this.datosEmpleoCargoComisionForm.get('ambitoPublico'));
     //this.filtrarEntes(value);
   }
 
   cambioAmbito(value: any) {
     console.log("ambito");
     console.log(value);
-    console.log(this.orden);
+    console.log(this.datosEmpleoCargoComisionForm.get('nivelOrdenGobierno'));
     //console.log(this.datosEmpleoCargoComisionForm.nivelOrdenGobierno.value);
     //this.filtrarEntes(value);
   }
