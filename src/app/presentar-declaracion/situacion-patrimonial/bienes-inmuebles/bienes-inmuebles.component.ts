354ebe50acaafc5292ef7684486e6a3f3e47af0d
= <=>
2025-04-02 13:46:33 -0600

    corrección de tercero.ninguno en bienesInmuebles

--- src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/bienes-inmuebles/bienes-inmuebles.component.ts
@@ -414,7 +414,7 @@ export class BienesInmueblesComponent implements OnInit {
       console.log(form.bienInmueble.length);
       if (this.tipoPersona ==="NINGUNO"){
         console.log("entra");
-        this.bienesInmueblesForm.get("bienInmueble.tercero[0].tipoPersona").setValue('FISICA');
+        this.bienesInmueblesForm.get("bienInmueble.tercero.tipoPersona").setValue('FISICA');
         /*console.log("llega1")
         if (this.editIndex === null) {
           console.log("llega if 1")
