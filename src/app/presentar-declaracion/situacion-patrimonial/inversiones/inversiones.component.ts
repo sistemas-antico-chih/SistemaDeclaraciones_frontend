2d11ce9a972c373991855863909b2f540e22f59a
= <=>
2025-04-04 11:05:46 -0600

    inversiones deja de cargar en modificar

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -424,10 +424,11 @@ export class InversionesComponent implements OnInit {
         switch (tipoInversion.clave) {
           case 'BANC':
             console.log("entra BANC")
-            console.log(this.opsBANC)
-            console.log("***: "+subTipoInversion.clave);
-            console.log("***: "+subTipoInversion.valor);
+            //console.log(this.opsBANC)
+            //console.log("***: "+subTipoInversion.clave);
+            //console.log("***: "+subTipoInversion.valor);
             this.subTipoInversion=subTipoInversion.clave, subTipoInversion.valor
+            console.log(subTipoInversion);
             //console.log(SubTipoInversionBancaria)
             //const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
             //const opt=['CEJE'];
