0e99845fde987d83a0e98335e6de6bb0edff7bc2
= <=>
2025-04-04 09:45:17 -0600

    inversiones deja de cargar en modificar

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -432,8 +432,8 @@ export class InversionesComponent implements OnInit {
             console.log("entra BANC")
             //console.log(this.subTipoBancariaCatalogo)
             //console.log(SubTipoInversionBancaria)
-            //const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
-            const opt=['CEJE'];
+            const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
+            //const opt=['CEJE'];
             console.log (opt[0]);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
             break;
