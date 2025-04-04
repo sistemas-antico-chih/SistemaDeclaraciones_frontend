1746c1c5229f00345a408243d5e585ea455f2c9a
= <=>
2025-04-04 09:14:53 -0600

    inversiones deja de cargar en modificar

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -413,8 +413,8 @@ export class InversionesComponent implements OnInit {
     if (tercero){
       console.log("tercero")
       console.log(tercero);
-      //const optionTercero = this.terceroCatalogo.filter((t: any) => t === tercero[0].tipoPersona);
-      const optionTercero='FISICA';
+      const optionTercero = this.terceroCatalogo.filter((t: any) => t === tercero[0].tipoPersona);
+      //const optionTercero='FISICA';
       // this.vehiculosForm.get('vehiculo.titular').setValue(findOption(this.titularBienCatalogo, titular[0]));
       this.inversionesCuentasValoresForm.get('inversion.tercero').setValue(optionTercero[0]);
     }
