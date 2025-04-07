8636cb4c5684ea250c0387fdb7bf339dd683a539
= <=>
2025-04-07 13:40:10 -0600

    prueba subtipoInversion

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -425,13 +425,13 @@ export class InversionesComponent implements OnInit {
           case 'BANC':
             console.log(subTipoInversion.clave);
             //const opt = this.subTipoBancariaCatalogo.filter((i: any) => i.clave === subTipoInversion.clave);
-            const opt = this.subTipoBancariaCatalogo.filter((i: any) => console.log(i.clave));
-            //const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
+            //const opt = this.subTipoBancariaCatalogo.filter((i: any) => console.log(i.clave));
+            const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
             const opt2 = this.opsBANC.filter((ban: any) => console.log(ban));
             console.log(opt);
             console.log("*****")
             console.log(opt2)
-            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
+            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').patchValue(opt);
             break;
           case 'FINV':
             const optFINV = this.opsFINV.filter((ban: any) => ban.clave === SubTipoInversionFondos.clave);
