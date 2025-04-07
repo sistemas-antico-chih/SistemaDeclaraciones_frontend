d12cc99006122bc35c8e83e2cf419c53def8c9e0
= <=>
2025-04-07 14:19:39 -0600

    prueba subtipoInversion

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -426,7 +426,7 @@ export class InversionesComponent implements OnInit {
             const opt = this.subTipoBancariaCatalogo.filter((i: any) => i.clave === subTipoInversion.clave);
             console.log("opt")
             console.log(opt)
-            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt);
+            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
             //this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt);
             break;
           case 'FINV':
