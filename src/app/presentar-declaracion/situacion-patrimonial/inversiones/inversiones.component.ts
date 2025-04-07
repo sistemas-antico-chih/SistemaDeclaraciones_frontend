e0a3bab65163a07be85431b86dd7f719818552e6
= <=>
2025-04-07 10:41:54 -0600

    prueba subtipoInversion

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -525,11 +525,14 @@ export class InversionesComponent implements OnInit {
     console.log("entra: "+value)
     console.log(this.subTipoBancariaCatalogo);
     const algo=this.inversionesCuentasValoresForm.get('inversion.subTipoInversion');
-    this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue([{"clave":"CEJE", "valor":"CUENTA EJE"}]);
+    this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue([{clave:"CEJE", valor:"CUENTA EJE"}]);
     const algo2=this.inversionesCuentasValoresForm.get('inversion').get ('subTipoInversion');
     console.log('algo: '+algo);
     console.log('xxxxx');
     console.log("this.subtipo: "+this.subTipo)
+    console.log("this.subtipo: "+this.subTipo.length)
+    console.log("this.subtipo: "+this.subTipo.clave)
+    console.log("this.subtipo: "+this.subTipo.valor)
     console.log('algo2: '+algo2.value);
     if (value === "NINGUNO") {
       this.inversionesCuentasValoresForm.get("inversion.tercero.nombreRazonSocial").clearValidators();
