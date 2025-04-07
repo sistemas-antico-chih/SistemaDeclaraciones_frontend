af38c743c8e6ab9fe885dd5bd447f5142c7f2485
= <=>
2025-04-07 09:14:31 -0600

    prueba subtipoInversion

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -521,8 +521,8 @@ export class InversionesComponent implements OnInit {
   }
 
   terceroChange(value: string) {
-    const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
-    const algo=this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
+    //const opt = this.opsBANC.filter((ban: any) => ban.clave === SubTipoInversionBancaria.clave);
+    const algo=this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue('opt[0]');
     console.log('algo: '+algo);
     if (value === "NINGUNO") {
       this.inversionesCuentasValoresForm.get("inversion.tercero.nombreRazonSocial").clearValidators();
