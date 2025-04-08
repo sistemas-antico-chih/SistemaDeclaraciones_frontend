839ac309f9c66ac2a106d346887987575a0e4fcd
= <=>
2025-04-08 09:29:56 -0600

    prueba subtipoInversion

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -423,19 +423,8 @@ export class InversionesComponent implements OnInit {
       if (subTipoInversion) {
         switch (tipoInversion.clave) {
           case 'BANC':
-            let opt = this.subTipoBancariaCatalogo.filter((i: any) => i.clave === subTipoInversion.clave);
-            //this.subTipo={clave:opt[0].clave, valor: opt[0].valor};
-            
-            //opt={clave:opt[0].clave, valor: opt[0].valor}
-            console.log("opt")
-            console.log(opt[0])
-            //this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').patchValue(opt[0]);
-            this.inversionesCuentasValoresForm.get('inversion').get('subTipoInversion').setValue(opt[0]);
-            //this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').patchValue(opt[0]);
-            //this.subTipo={opt};
-            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
-            this.subTipo=opt[0];
-            console.log(this.subTipo);
+            let optBANC = this.subTipoBancariaCatalogo.filter((i: any) => i.clave === subTipoInversion.clave);
+            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optBANC[0]);
             break;
           case 'FINV':
             const optFINV = this.opsFINV.filter((ban: any) => ban.clave === SubTipoInversionFondos.clave);
