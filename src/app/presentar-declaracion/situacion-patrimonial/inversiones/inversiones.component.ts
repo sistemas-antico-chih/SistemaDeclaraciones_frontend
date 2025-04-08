85ead23b241521bdb9fdae9de85b70793197e259
= <=>
2025-04-08 11:09:52 -0600

    precarga subtipoInversion, el sistema se encuentra funcionando y precargando todos los campos en todos los apartados

--- src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/inversiones/inversiones.component.ts
@@ -426,33 +426,28 @@ export class InversionesComponent implements OnInit {
             const opt = this.opsBANC.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(opt[0]);
             break;
-            /*let optBANC = this.subTipoBancariaCatalogo.filter((i: any) => i.clave === subTipoInversion.clave);
-            this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optBANC[0].valor);
-            console.log(optBANC[0].valor);
-            this.sub=optBANC[0].valor;
-            break;*/
           case 'FINV':
-            const optFINV = this.opsFINV.filter((ban: any) => ban.clave === SubTipoInversionFondos.clave);
+            const optFINV = this.opsFINV.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optFINV[0]);
             break;
           case 'ORPM':
-            const optORPM = this.opsORPM.filter((ban: any) => ban.clave === SubTipoInversionOrganizaciones.clave);
+            const optORPM = this.opsORPM.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optORPM[0]);
             break;
           case 'POMM':
-            const optPOMM = this.opsPOMM.filter((ban: any) => ban.clave === SubTipoInversionMetales.clave);
+            const optPOMM = this.opsPOMM.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optPOMM[0]);
             break;
           case 'SEGR':
-            const optSEGR = this.opsSEGR.filter((ban: any) => ban.clave === SubTipoInversionSeguros.clave);
+            const optSEGR = this.opsSEGR.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optSEGR[0]);
             break;
           case 'VBUR':
-            const optVBUR = this.opsVBUR.filter((ban: any) => ban.clave === SubTipoInversionValores.clave);
+            const optVBUR = this.opsVBUR.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optVBUR[0]);
             break;
           case 'AFOT':
-            const optAFOT = this.opsAFOT.filter((ban: any) => ban.clave === SubTipoInversionAfores.clave);
+            const optAFOT = this.opsAFOT.filter((ban: any) => ban.clave === subTipoInversion.clave);
             this.inversionesCuentasValoresForm.get('inversion.subTipoInversion').setValue(optAFOT[0]);
             break;
           default:
