5d57d97a8abf67e4747ba30b3d5cca7c6d79b028
= <=>
2025-04-22 09:41:51 -0600

    limitar caracteres en numero exterior / interipor de domicilio declarante

--- src/app/presentar-declaracion/situacion-patrimonial/domicilio-declarante/domicilio-declarante.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/domicilio-declarante/domicilio-declarante.component.ts
@@ -80,8 +80,8 @@ export class DomicilioDeclaranteComponent implements OnInit {
     this.domicilioDeclaranteForm = this.formBuilder.group({
       domicilioMexico: this.formBuilder.group({
         calle: [null, [Validators.required, Validators.pattern(/^\S.*$/)]],
-        numeroExterior: [null, [Validators.required, Validators.pattern(/^\S{6}$/)]],
-        numeroInterior: [null, [Validators.pattern(/^\S{6}$/)]],
+        numeroExterior: [null, [Validators.required, Validators.pattern(/^\S{1,6}$/)]],
+        numeroInterior: [null, [Validators.pattern(/^\S{1,6}$/)]],
         coloniaLocalidad: [null, [Validators.required, Validators.pattern(/^\S.*$/)]],
         municipioAlcaldia: [{ disabled: true, value: null }, [Validators.required]],
         entidadFederativa: [null, [Validators.required]],
