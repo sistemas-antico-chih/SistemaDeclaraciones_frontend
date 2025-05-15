e58a04a483adba4538ead39c6e1a00bfa979eef5
= <=>
2025-05-15 12:01:43 -0600

    correccion de domicilio en datos pareja

--- src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-pareja/datos-pareja.component.ts
@@ -545,7 +545,7 @@ export class DatosParejaComponent implements OnInit {
       this.datosParejaForm.get("curp").updateValueAndValidity();
       this.datosParejaForm.get("curp").disable();
     }
-    console.log("Requerido", this.datosParejaForm.errors);
+    //console.log("Requerido", this.datosParejaForm.errors);
   }
   
   ngOnInit(): void {
