98fb63eb5c38dccb1283eb7150faf3983f3ba7f0
= <=>
2025-06-05 11:48:39 -0600

    documento obtenido en datos curriculares

--- src/app/presentar-declaracion/situacion-patrimonial/datos-curriculares/datos-curriculares.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-curriculares/datos-curriculares.component.ts
@@ -360,8 +360,8 @@ export class DatosCurricularesComponent implements OnInit {
   cambioEstatus(valorEstatus: any) {
     console.log(valorEstatus);
     if (valorEstatus === 'FINALIZADO'){
-      console.log(this.datosCurricularesDeclaranteForm.get('escolaridad').get('nivel'))
-      console.log(this.datosCurricularesDeclaranteForm.get('escolaridad').get('documentoObtenido'))
+      console.log(this.datosCurricularesDeclaranteForm.get('escolaridad').get('nivel').get('valor').value)
+      console.log(this.datosCurricularesDeclaranteForm.get('escolaridad').get('documentoObtenido').value)
     }
   }
 }
