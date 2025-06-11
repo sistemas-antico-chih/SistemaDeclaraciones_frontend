02d9e9db735403d71ce84ae9807fb8af0285e9cf
= <=>
2025-06-11 11:19:14 -0600

    agregar aviso

--- src/app/aviso/datos-generales/datos-generales.component.ts
+++ src/app/aviso/datos-generales/datos-generales.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, NgModule } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
