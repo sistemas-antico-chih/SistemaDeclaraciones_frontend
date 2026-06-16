697cb5f870ec7e5b7dbbcad574da4747baace440
= <=>
2026-06-16 12:38:56 -0600

    agregar campo extemporaneo

--- src/app/presentar-declaracion/situacion-patrimonial/datos-generales/datos-generales.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-generales/datos-generales.component.ts
@@ -217,7 +217,7 @@ export class DatosGeneralesComponent implements OnInit {
 
       this.declaracionId = data?.declaracion._id;
       this.anio_ejercicio = data?.declaracion.anioEjercicio;
-      this.esExtemporanea = data?.declaracion.esExtemporanea || false;
+      this.esExtemporanea = data?.esExtemporanea || false;
 
       if (data.declaracion.datosGenerales === null) {
         this.isFromPreviousRecord = true;
