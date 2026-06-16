a9e2b67375e6430d152540bace57808c271e4b6d
= <=>
2026-06-16 12:24:38 -0600

    agregar campo extemporaneo

--- src/app/presentar-declaracion/situacion-patrimonial/datos-generales/datos-generales.component.ts
+++ src/app/presentar-declaracion/situacion-patrimonial/datos-generales/datos-generales.component.ts
@@ -294,7 +294,7 @@ export class DatosGeneralesComponent implements OnInit {
       const declaracion = {
         datosGenerales: this.finalForm,
         anioEjercicio: this.anio_ejercicio,
-        this.extemporanea: this.esExtemporanea
+        this.esExtemporanea: this.esExtemporanea
       };
 
       const { errors } = await this.apollo
