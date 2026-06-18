4208384ccad89369ad5442cb25381b1cb39a47b7
= <=>
2026-06-18 13:07:56 -0600

    agregar 'notaAclaratoria'

--- src/models/declaracion/declaracion-metadata.model.ts
+++ src/models/declaracion/declaracion-metadata.model.ts
@@ -2,6 +2,7 @@ import { TipoDeclaracion } from './types';
 import { User } from '@models/user';
 
 export interface DeclaracionMetadata {
+  _id: string;
   firmada: boolean;
   declaracionCompleta: boolean;
   tipoDeclaracion: TipoDeclaracion;
