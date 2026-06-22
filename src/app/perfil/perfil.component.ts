8a3081edf57d958b9ea2bfcb822f70173af005fd
= <=>
2026-06-22 14:18:35 -0600

    agregar validacion de correo primer paso

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -100,6 +100,7 @@ export class PerfilComponent implements OnInit {
         '',
         [
           Validators.required,
+          Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
           Validators.email
         ],
       ],
