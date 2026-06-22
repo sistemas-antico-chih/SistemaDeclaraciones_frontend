429cb89ea38fff1938a18be052c2bdb3d9491519
= <=>
2026-06-22 12:09:43 -0600

    agregar validacion de correo primer paso

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -57,9 +57,7 @@ export class SignupComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.signupForm.valueChanges.subscribe(() => {
-      console.log(this.signupForm.errors);
-    });
+
   }
 
   ngOnDestroy() { }
@@ -126,55 +124,27 @@ export class SignupComponent implements OnInit, OnDestroy {
   }
 
   private createForm() {
-    this.signupForm.valueChanges.subscribe(() => {
-      this.signupForm.updateValueAndValidity({ onlySelf: false, emitEvent: false });
-    });
     this.signupForm = this.formBuilder.group({
-      //nombre: ['', Validators.required,
-      nombre: ['',
-        Validators.pattern(/^[a-z\s\u00E0-\u00FC\u00f1\u00d1]*$/i),
-      ],
-      primerApellido: ['',
-        Validators.pattern(/^[a-z\s\u00E0-\u00FC\u00f1\u00d1]*$/i),
-      ],
+      nombre: ['', Validators.pattern(/^[a-z\s\u00E0-\u00FC\u00f1\u00d1]*$/i)],
+      primerApellido: ['', Validators.pattern(/^[a-z\s\u00E0-\u00FC\u00f1\u00d1]*$/i)],
       segundoApellido: ['', Validators.pattern(/^[a-z\s\u00E0-\u00FC\u00f1\u00d1]*$/i)],
+
       username: [
         '',
         [
           Validators.required,
-          Validators.pattern(
-            /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i
-          ),
-        ],
-      ],
-      confirmarCorreo: [
-        '',
-        [
-          Validators.required
+          Validators.pattern(/* regex correo */)
         ]
       ],
-      curp: [
-        '',
-        [
-          Validators.required,
-          validarCURP,
-          Validators.pattern(
-            /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/i
-          ),
-        ],
-      ], updatedOn: 'change',
-      rfc: [
-        '',
-        [
-          Validators.required,
-          validarRFC,
-          Validators.pattern(
-            /^([A-ZÑ&]{3,4}) ?(?:- ?)?(\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])) ?(?:- ?)?([A-Z\d]{2})([A\d])$/i
-          ),
-        ],
-      ],
+
+      confirmarCorreo: ['', [Validators.required]],
+
+      curp: ['', [Validators.required, validarCURP]],
+      rfc: ['', [Validators.required, validarRFC]],
+
       contrasena: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
-      confirmarContrasena: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
+      confirmarContrasena: ['', [Validators.required]],
+
       institucion: [{ disabled: true, value: null }, [Validators.required]],
     },
       {
